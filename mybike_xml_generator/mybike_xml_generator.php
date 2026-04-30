eacc3295bc24c215f637c0ef42370964f7179a70
Laimonas-Mituzas <laimonas.mituzas@gmail.com>
2026-04-30 09:35:48 +0300

    Bump module version to 2.0.0

--- mybike_xml_generator/mybike_xml_generator.php
+++ mybike_xml_generator/mybike_xml_generator.php
@@ -13,7 +13,7 @@ class Mybike_xml_generator extends Module
     {
         $this->name          = 'mybike_xml_generator';
         $this->tab           = 'administration';
-        $this->version       = '1.0.0';
+        $this->version       = '2.0.0';
         $this->author        = 'Augu su Presta';
         $this->need_instance = 0;
         $this->bootstrap     = true;
